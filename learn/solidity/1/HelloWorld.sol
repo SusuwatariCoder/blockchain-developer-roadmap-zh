5b20210d321b8c8b350d08b2331a41b4a49318ba
aturx <pywizard6261@gmail.com>
2021-02-02 07:06:19 +0800

    solidity

---
+++ learn/solidity/1/HelloWorld.sol
@@ -0,0 +1,5 @@
+contract HelloWorld {
+    function myFirst(address _MyAddress) public {
+        // DO 
+    }
+}
---
+++ learn/solidity/README.md
@@ -0,0 +1,5 @@
+# Solidity 开发实战 
+
+## 第1章、  以太坊智能合约开发环境
+
+## 第2章、  Solidity 开发HelloWorld 合约
